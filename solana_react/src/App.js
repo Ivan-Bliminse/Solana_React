ee1f4ec36780ac46b3345ac5f1c9aa9d79f4e606
Ivan-Bliminse <arturgorbunov3@gmail.com>
2022-07-05 15:19:38 +0300

    Solana Web React ::: without video

--- solana_react/src/App.js
+++ solana_react/src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import Cards from './Components/Cards';
+import ContainerGreen from './Components/ContainerGreen';
+import Decentralized from './Components/Decentralized';
+import DoubleBlock from './Components/DoubleBlock';
+import Grid from './Components/Grid';
+import Head from './Components/Head';
+import Navigation from './Components/Navigation';
+import Scale from './Components/Scale';
 
 function App() {
+
+  const scaleImg = document.querySelector('.scaleImage')
+  const cardOne = document.querySelector('.cardOne')
+  const cardTwo = document.querySelector('.cardTwo')
+
+  function scroll() {
+    const scrolled = window.scrollY + window.innerHeight
+    // Scale Image
+    const scaleImgTrue = (scaleImg.offsetTop + scaleImg.offsetHeight / 3) < scrolled
+    const scaleImgFinish = (scaleImg.offsetTop + scaleImg.offsetHeight) > window.scrollY
+
+    // Cards One and Two
+    const Card1 = (cardOne.offsetTop + cardOne.offsetHeight / 3.5) < scrolled
+    const Card1Finish = (cardOne.offsetTop + cardOne.offsetHeight) > window.scrollY
+
+    console.log(Card1, Card1Finish)
+
+    if (scaleImgTrue && scaleImgFinish) {
+      scaleImg.classList.add('active')
+    } else {
+      scaleImg.classList.remove('active')
+    }
+
+    if (Card1 && Card1Finish) {
+      cardOne.classList.add('activeRight')
+      cardTwo.classList.add('activeLeft')
+    } else {
+      cardOne.classList.remove('activeRight')
+      cardTwo.classList.remove('activeLeft')
+    }
+
+  }
+
+
+  window.addEventListener('scroll', scroll)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navigation />
+      <Head />
+      <ContainerGreen />
+      <DoubleBlock />
+      <Scale />
+      <Cards />
+      <Decentralized />
+      <Grid />
     </div>
   );
 }
