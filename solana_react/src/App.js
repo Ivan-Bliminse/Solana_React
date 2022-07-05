2a767cae69c4f405fd0809bbbb3528d1b9a97f0d
Ivan-Bliminse <arturgorbunov3@gmail.com>
2022-07-05 15:45:02 +0300

    useEffect offset bug Fix

--- solana_react/src/App.js
+++ solana_react/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Cards from './Components/Cards';
 import ContainerGreen from './Components/ContainerGreen';
 import Decentralized from './Components/Decentralized';
@@ -9,36 +10,39 @@ import Scale from './Components/Scale';
 
 function App() {
 
-  const scaleImg = document.querySelector('.scaleImage')
-  const cardOne = document.querySelector('.cardOne')
-  const cardTwo = document.querySelector('.cardTwo')
+  useEffect(() => {
+    const scaleImg = document.querySelector('.scaleImage')
+    const cardOne = document.querySelector('.cardOne')
+    const cardTwo = document.querySelector('.cardTwo')
 
-  function scroll() {
-    const scrolled = window.scrollY + window.innerHeight
-    // Scale Image
-    const scaleImgTrue = (scaleImg.offsetTop + scaleImg.offsetHeight / 3) < scrolled
-    const scaleImgFinish = (scaleImg.offsetTop + scaleImg.offsetHeight) > window.scrollY
+    function scroll() {
+      const scrolled = window.scrollY + window.innerHeight
+      // Scale Image
+      const scaleImgTrue = (scaleImg.offsetTop + scaleImg.offsetHeight / 3) < scrolled
+      const scaleImgFinish = (scaleImg.offsetTop + scaleImg.offsetHeight) > window.scrollY
 
-    // Cards One and Two
-    const Card1 = (cardOne.offsetTop + cardOne.offsetHeight / 3.5) < scrolled
-    const Card1Finish = (cardOne.offsetTop + cardOne.offsetHeight) > window.scrollY
+      // Cards One and Two
+      const Card1 = (cardOne.offsetTop + cardOne.offsetHeight / 3.5) < scrolled
+      const Card1Finish = (cardOne.offsetTop + cardOne.offsetHeight) > window.scrollY
 
-    if (scaleImgTrue && scaleImgFinish) {
-      scaleImg.classList.add('active')
-    } else {
-      scaleImg.classList.remove('active')
+      if (scaleImgTrue && scaleImgFinish) {
+        scaleImg.classList.add('active')
+      } else {
+        scaleImg.classList.remove('active')
+      }
+
+      if (Card1 && Card1Finish) {
+        cardOne.classList.add('activeRight')
+        cardTwo.classList.add('activeLeft')
+      } else {
+        cardOne.classList.remove('activeRight')
+        cardTwo.classList.remove('activeLeft')
+      }
+
     }
+    window.addEventListener('scroll', scroll)
 
-    if (Card1 && Card1Finish) {
-      cardOne.classList.add('activeRight')
-      cardTwo.classList.add('activeLeft')
-    } else {
-      cardOne.classList.remove('activeRight')
-      cardTwo.classList.remove('activeLeft')
-    }
-
-  }
-  window.addEventListener('scroll', scroll)
+  }, [])
 
 
   return (
